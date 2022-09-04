f6aafebaef5914aa673f0d3c95ce3a2a91345fa4
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-09-04 17:38:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
 
 app = Flask(__name__)
 
-@app.route('/puppet')
-def hello_world():
-	return 'Hello World'
+def fetchUser(gender, nationality):
+    res = requests.get(f'https://randomuser.me/api/?gender={gender}&nat={nationality}&password=upper,lower,number,special,8-12&format=json')
+    response = json.loads(res.text)
+    return response
 
-res = requests.get('paste your link here')
-response = json.loads(res.text)
+@app.route('/puppet/', methods=['GET'])
+def hello_world():
+    gender = request.args.get('gender', default = '', type = str)
+    nationality = request.args.get('nationality', default = 'us', type = str)
+    return fetchUser(gender, nationality)
+
+@app.route('/seed/<seed>', methods=['GET'])
+def seed(seed):
+    return json.loads(requests.get(f'https://randomuser.me/api/?seed={seed}&nat=in&format=json&password=upper,lower,number,special,8-12').text)
 
 # main driver function
 if __name__ == '__main__':
