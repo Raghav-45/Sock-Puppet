0b9af6d53d1aca9f6b7ffe491cfd4e895c719b9d
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-09-04 19:30:47 +0530

    Started Implementing function to save Puppet Data to a JSON File

--- app.py
+++ app.py
@@ -13,11 +13,29 @@ def fetchUser(gender, nationality):
 def hello_world():
     gender = request.args.get('gender', default = '', type = str)
     nationality = request.args.get('nationality', default = 'us', type = str)
-    return fetchUser(gender, nationality)
+    data = fetchUser(gender, nationality)
+    write_json(data)
+    return data['results']
 
+def write_json(new_data, filename='person.json'):
+    with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Custom JSON Object
+        new_data = ({new_data['info']['seed']: [new_data['results'][0]]})
+        # Join new_data with file_data inside emp_details
+        file_data["seed_details"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+    
 @app.route('/seed/<seed>', methods=['GET'])
 def seed(seed):
-    return json.loads(requests.get(f'https://randomuser.me/api/?seed={seed}&nat=in&format=json&password=upper,lower,number,special,8-12').text)
+    with open('person.json','r') as file:
+        file_data = json.load(file)[str(seed)]
+        return file_data
+    # return json.loads(requests.get(f'https://randomuser.me/api/?seed={seed}&nat=in&format=json&password=upper,lower,number,special,8-12').text)
 
 # main driver function
 if __name__ == '__main__':
