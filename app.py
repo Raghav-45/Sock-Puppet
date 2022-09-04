8a3eb188d9f21ee3db0dcb4a8cf80bccd131ec41
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-09-04 20:18:56 +0530

    Store Seeds
    Added Functionality to store seeds of Puppet Profiles

--- app.py
+++ app.py
@@ -9,14 +9,6 @@ def fetchUser(gender, nationality):
     response = json.loads(res.text)
     return response
 
-@app.route('/puppet/', methods=['GET'])
-def hello_world():
-    gender = request.args.get('gender', default = '', type = str)
-    nationality = request.args.get('nationality', default = 'us', type = str)
-    data = fetchUser(gender, nationality)
-    write_json(data)
-    return data['results']
-
 def write_json(new_data, filename='person.json'):
     with open(filename,'r+') as file:
         # First we load existing data into a dict.
@@ -24,18 +16,27 @@ def write_json(new_data, filename='person.json'):
         # Custom JSON Object
         new_data = ({new_data['info']['seed']: [new_data['results'][0]]})
         # Join new_data with file_data inside emp_details
-        file_data["seed_details"].append(new_data)
+        # file_data["seed_details"].append(new_data)
+        file_data["seed_details"].update(new_data)
         # Sets file's current position at offset.
         file.seek(0)
         # convert back to json.
         json.dump(file_data, file, indent = 4)
+
+@app.route('/puppet/', methods=['GET'])
+def hello_world():
+    gender = request.args.get('gender', default = '', type = str)
+    nationality = request.args.get('nationality', default = 'us', type = str)
+    data = fetchUser(gender, nationality)
+    write_json(data)
+    return data
     
 @app.route('/seed/<seed>', methods=['GET'])
 def seed(seed):
     with open('person.json','r') as file:
-        file_data = json.load(file)[str(seed)]
-        return file_data
-    # return json.loads(requests.get(f'https://randomuser.me/api/?seed={seed}&nat=in&format=json&password=upper,lower,number,special,8-12').text)
+        file_data = json.load(file)
+        return file_data['seed_details'][seed]
+    
 
 # main driver function
 if __name__ == '__main__':
